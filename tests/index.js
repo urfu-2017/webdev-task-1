bf85b0023730ecd410cb060700ad38d578aa1f48
Igor Savichev <savichev@yandex-team.ru>
2018-03-01 18:00:11 +0500

    Костяк (#1)

---
+++ tests/index.js
@@ -0,0 +1,12 @@
+'use strict';
+const request = require('supertest');
+
+const app = require('../index');
+
+describe('GET /', () => {
+    it('respond with html page', done => {
+        request(app)
+            .get('/')
+            .expect(200, done);
+    });
+});
