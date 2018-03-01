eaa2c3ac2d17562a906a64f13c7a724944b3b64e
Alexey <a.dubrovin@skbkontur.ru>
2018-03-02 00:38:02 +0500

    add simple index.html register hbs

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
+
+const path = require('path');
+
+
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+const viewsDir = path.join(__dirname, 'views');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsDir);
 
 module.exports = app;
