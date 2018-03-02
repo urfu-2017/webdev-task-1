0d0add96ce282bc1a9d935f9ba1cd9f6cdd4bf08
Ilya Kulygin <ruserixsian@gmail.com>
2018-03-02 20:36:55 +0500

    Добавлен генератор шаблонов BH

---
+++ configureTemplateEngine.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const bh = require('bh');
+
+module.exports = app => {
+    const blockPath = path.resolve(__dirname, 'views', 'blocks');
+    const blocks = fs.readdirSync(blockPath);
+
+    app.engine('js', (filepath, options, callback) => {
+        // Для автообновления шаблона, на каждый запрос создаём новый экземпляр BH
+        // и очищаем кэш модулей node.js
+        const render = new bh.BH();
+        const renderFunction = require(filepath);
+
+        blocks.forEach(blockFile => {
+            require(path.resolve(blockPath, blockFile))(render);
+        });
+
+        Object.keys(require.cache).forEach(key => {
+            delete require.cache[key];
+        });
+
+        if (renderFunction) {
+            const bemjson = renderFunction(options);
+            const rendered = render.apply(bemjson);
+
+            return callback(callback(null, rendered));
+        }
+
+        callback(new Error('View file not found'));
+    });
+
+    app.set('views', './views');
+    app.set('view engine', 'js');
+};
