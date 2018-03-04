88d5b27fe1e55b66a0fb387c10f529e5a072f50e
eyebrow elite <wivoceraku@p33.org>
2018-03-04 21:46:12 +0500

    +Weather

--- models/datatypes.js
+++ models/datatypes.js
@@ -1,24 +1,25 @@
 const Type = require('union-type')
 
-const NewsCategory = Type({ 
+const NewsCategory = Type({ NewsCategory: {
     title: String,
     icon: String,
     urlShortName: String 
-})
+}})
 
-const NewsItem = Type({ 
+const NewsItem = Type({ NewsItem: {
     title: String,
     publishedAt: Date,
     description: String,
     source: String,
     category: NewsCategory
-})
+}})
 
-const WeatherInfo = Type({ 
+const WeatherInfo = Type({ WeatherInfo : {
     city: String,
+    weatherState: String,
     tempCelsius: Number,
     windMps: Number,
     day: Date
-})
+}})
 
 module.exports = { NewsCategory, NewsItem, WeatherInfo }
--- models/weather.js
+++ models/weather.js
@@ -1,4 +1,5 @@
 const { WeatherInfo } = require('./datatypes')
+const MetaWeather = require('metaweather')
 
 
 class Weather {
@@ -6,12 +7,46 @@ class Weather {
         this.metaWeather = metaWeather
     }
 
-    getWeatherByQuery(query) {
-        return WeatherInfo.WeatherInfoOf({  });
+    async getWoeidByQuery(query) {
+        const response = await this.metaWeather.search().query(query)
+        if (response.body && response.body[0])
+            return response.body[0].woeid
     }
 
-    getWeatherByLocation(lattitude, longitude) {
-        return WeatherInfo.WeatherInfoOf({  });
+    async getWoeidByLocation(lattitude, longitude) {
+        const response = await this.metaWeather.search().latLon({ lat: lattitude, lon: longitude })
+        if (response.body && response.body[0])
+            return response.body[0].woeid
+        return null
+    }
+
+    async getWeatherByWoeid(woeid) {
+        if (woeid) {
+            const response = await this.metaWeather.location(woeid)
+            if (response.body && response.body.consolidated_weather)
+                return response.body.consolidated_weather.map(x => WeatherInfo.WeatherInfoOf({
+                    city: response.body.title,
+                    weatherState: x.weather_state_abbr,
+                    tempCelsius: x.the_temp,
+                    windMps: x.wind_speed,
+                    day: new Date(x.applicable_date)
+                }))
+        }
+        return null
+    }
+
+    async getWeatherByQuery(query) {
+        const woeid = await this.getWoeidByQuery(query)
+        return await this.getWeatherByWoeid(woeid)
+    }
+
+    async getWeatherByLocation(lattitude, longitude) {
+        const woeid = await this.getWoeidByLocation(lattitude, longitude)
+        return await this.getWeatherByWoeid(woeid)
+    }
+
+    static getInstance() {
+        return new Weather(new MetaWeather);
     }
 }
 
