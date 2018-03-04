900c984c933e2393af7305ac6c277e4c8217e5ba
eyebrow elite <wivoceraku@p33.org>
2018-03-04 22:28:04 +0500

    +news

--- models/news.js
+++ models/news.js
@@ -6,11 +6,27 @@ class News {
     }
 
     getCategories() {
-        return []
-    }   
-    
-    getNews(category, page=0) {
-        return []
+        return [
+            ['business', 'Бизнес'],
+            ['entertainment', 'Развлеченie'],
+            ['general', 'Генер@л'],
+            ['health', 'HP-шка'],
+            ['science', 'Науkka'],
+            ['sports', 'Спорт'],
+            ['technology', 'Технолоgy'],
+        ].map(([urlShortName, title]) => NewsCategory.NewsCategoryOf({ urlShortName, title, icon: urlShortName }))
+    }
+
+    async getNews(category) {
+        const response = await this.newsApi.v2.topHeadlines({ category: category.urlShortName, language: 'ru' })
+
+        return response.articles.map(x => NewsItem.NewsItemOf({
+            title: x.title,
+            publishedAt: new Date(x.publishedAt),
+            description: x.description || '',
+            source: x.url,
+            category
+        }))
     }
 }
 
---
+++ tests/news.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const { News } = require('../models/news')
+const { NewsItem, NewsCategory } = require('../models/datatypes')
+const NewsApi = require('newsapi')
+const assert = require('assert')
+
+
+describe('news', () => {
+    const testApiKey = 'af406ce76d00443f90f40b9a2e5f2da4'
+    const sut = new News(new NewsApi(testApiKey))
+
+    it('has some categories', () => {
+        const categories = sut.getCategories()
+
+        categories.forEach(NewsCategory.case({
+            NewsCategory: () => {},
+            _: () => assert.fail('This isnt a NewsCategory')
+        }));
+        assert.ok(sut.getCategories().length > 0)
+    })
+
+    it('has news for each category', async () => {
+        const categories = sut.getCategories()
+        
+        const results = await Promise.all(categories.map(x => sut.getNews(x)))
+        console.info(results[0][0])
+        assert.ok(results.every(x => x.length > 0))
+        results.forEach(x => x.forEach(NewsItem.case({
+            NewsItem: () => {},
+            _: () => assert.fail('This isnt a NewsItem')
+        })))
+    })
+})
