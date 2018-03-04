f8b800a4de39bc13801c4fd01b29bf5fdec2335c
eyebrow elite <wivoceraku@p33.org>
2018-03-04 22:58:10 +0500

    sample app

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-'use strict';
-const express = require('express');
+'use strict'
+const express = require('express')
+const exphbs  = require('express-handlebars')
 
-const app = express();
+const app = express()
 
-module.exports = app;
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'html5' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home',
+        totle: 'Boem'
+    })
+})
+
+module.exports = app
+
+if (require.main === module) {
+    app.listen(8080, () => console.log('Running on localhost:8080'))
+}
