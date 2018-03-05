5db9c13361d06c685bb75b6e26d15f4996d70b1e
eyebrow elite <wivoceraku@p33.org>
2018-03-05 16:02:37 +0500

    helpers

--- index.js
+++ index.js
@@ -6,11 +6,34 @@ const { wrap } = require('async-middleware')
 const moment = require('moment')
 
 const displayRussianDate = date => moment(date).locale('ru').format('Do MMMM')
+const mwStateToIcon = state => 'icofont-' + ({
+    sn: 'snowy',
+    sl: 'snowy-rainy',
+    h: 'hail',
+    t: 'rainy-thunder',
+    hr: 'rainy',
+    lr: 'rainy',
+    s: 'rainy-sunny',
+    hc: 'clouds',
+    lc: 'cloudy',
+    c: 'sun-alt'
+}[state])
+const head = list => list[0]
+const tail = list => list.slice(1)
+const round = (digits, number) => number.toFixed(digits)
+
+const fmtTemp = temp => (temp == 0 ? '0' : ((temp > 0 ? '+' : '-') + temp.toFixed(1))) + ' ℃'
+const fmtWind = wind => `${wind.toFixed(2)} м/c`
+
 
 const app = express()
-app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'html5', helpers: { displayRussianDate } }))
+app.engine('hbs', exphbs({ 
+    extname: '.hbs',
+    defaultLayout: 'html5',
+    helpers: { displayRussianDate, head, tail, mwStateToIcon, fmtTemp, fmtWind } 
+}))
 app.set('view engine', 'hbs')
-app.locals.siteName = 'LocalExtreme'
+app.locals.siteName = 'Local Extremum'
 app.use(express.static('public'))
 
 app.use(wrap(forecastMiddleware))
