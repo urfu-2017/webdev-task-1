e128c69cd55771432efab63ad6d16498d657bf84
Aleksandr Chernyshev <sanocher@gmail.com>
2018-03-06 02:48:29 +0500

    fast commit

---
+++ repository/commonRepository.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const request = require('request');
+
+exports.getBodyByUrl = (url) => {
+    return new Promise( (resolve, reject) => {
+        request(url, (error, response, body) => {
+            let list;
+            if (!error && response.statusCode == 200) {
+                let result = JSON.parse(body);
+                resolve(result);                   
+            } else {
+                reject(error);
+            }
+        });    
+    })
+}
