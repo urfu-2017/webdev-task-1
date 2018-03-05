57475fde49dca3fa395bec0ff14da0122579e66c
eyebrow elite <wivoceraku@p33.org>
2018-03-05 16:02:28 +0500

    ctl

--- controllers/index.js
+++ controllers/index.js
@@ -6,46 +6,37 @@ const weather = Weather.getInstance()
 
 const forecastMiddleware = async (req, res, next) => {
     const { query, lat, lon } = req.query
-    if (!query && (!lat || !lon))
-        res.redirect('?query=london')
-    else {
-        const forecast = await (query ? weather.getWeatherByQuery(query) : weather.getWeatherByLocation(lat, lon))
+    const forecast = await ((lat && lon) ? weather.getWeatherByLocation(lat, lon) : weather.getWeatherByQuery(query || 'london'))
 
-        if (forecast === null && query !== 'london')
-            res.redirect('?query=london')
-        else {
-            req.forecast = forecast
-            next()
-        }
-    }
-    
+    req.forecast = forecast
+    next()
 }
 
 const index = async (req, res) => {
     const categories = news.getCategories()
 
     res.render('home', {
-        title: 'main page',
+        title: 'Главная',
         categories,
         forecast: req.forecast
     })
 }
 
 const getCategory = async (req, res) => {
-    const { categoryName } = req.params
-    const [category] = news.getCategories().filter(x => x.urlShortName === categoryName)
+    const categoryShortName = req.params.category
+    const [category] = news.getCategories().filter(x => x.urlShortName === categoryShortName)
 
     if (category) {
-        const news = await news.getNews(category)
+        const newsItems = await news.getNews(category)
 
         res.render('category',  {
             title: category.title,
             forecast: req.forecast,
             category: category,
-            news
+            news: newsItems
         })
     }
-    else res.send(404)
+    else res.sendStatus(404)
 }
 
 module.exports = { forecastMiddleware, index, getCategory }
