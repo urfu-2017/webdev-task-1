52c273c525778713e2078a12d2539002ceb4a7ab
Alexey <a.dubrovin@skbkontur.ru>
2018-03-05 23:47:40 +0500

    add 404

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const { getPage: info, news } = require('./controllers/info');
+const { error404 } = require('./controllers/error');
 
 module.exports = app => {
     app.get('/', info);
 
     app.get('/news/:category([a-z]+)', news);
+
+    app.all('*', error404);
 };
