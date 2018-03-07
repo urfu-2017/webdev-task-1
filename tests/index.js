4abd643703fab882fda4444664d16cd472a88ca4
eyebrow elite <wivoceraku@p33.org>
2018-03-07 17:30:34 +0500

    simple cat test + 404

--- tests/index.js
+++ tests/index.js
@@ -10,3 +10,20 @@ describe('GET /', () => {
             .expect(200, done);
     });
 });
+
+
+describe('GET /business', () => {
+    it('respond with html page', done => {
+        request(app)
+            .get('/business')
+            .expect(200, done);
+    });
+});
+
+describe('GET /BAD_BAD_BAD', () => {
+    it('should give 404', done => {
+        request(app)
+            .get('/BAD_BAD_BAD')
+            .expect(404, done);
+    });
+});
