6287709205102f73cbc04eb24c50d99d0693c58a
Reshetnikov Artem <mr.reshetnikov.artem.2017@mail.ru>
2018-03-07 22:04:34 +0500

    + немного погоды, - немного MVC

--- controllers/cathegories.js
+++ controllers/cathegories.js
@@ -1,16 +1,26 @@
 'use strict';
-
 const cathegories = require('../mocks/cathegories');
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('f8c7968376984c3db60b4759fa20cfe4');
-
+const fetch = require('node-fetch');
+const baseUrl = 'https://www.metaweather.com/api/location/44418/';
+const Weather = require('../models/weather').Weather;
 // Каждый контроллер (controller) обычно экспортирует
 // несколько функций-действий (actions)
 
 exports.listCathegories = (req, res) => {
-    // Объединяем данные специфичные для контроллера с общими данными
-    const data = { cathegories };
-    res.render('index', data);
+    fetch(baseUrl).then(
+        function (u) {
+            return u.json();
+        })
+        .then(
+            function (weatherData) {
+                let weather = new Weather(weatherData);
+                // console.info(weather);
+                const data = { cathegories, weather };
+                res.render('index', data);
+            }
+        );
 };
 
 exports.listNews = (req, res) => {
---
+++ models/weather.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const baseUrl = 'https://www.metaweather.com';
+
+class Weather {
+    constructor(weatherData) {
+        let weatherList = [];
+        weatherData.consolidated_weather.forEach(o => {
+            let theTemp = Math.round(o.the_temp);
+            let windSpeed = Math.round(o.wind_speed);
+            weatherList.push({ theTemp, windSpeed });
+        });
+        this.title = weatherData.title;
+        this.weatherList = weatherList;
+        let svgFileName = weatherData.consolidated_weather[0].weather_state_abbr;
+        this.currentTemp = this.weatherList[0].theTemp;
+        this.currentWind = this.weatherList[0].windSpeed;
+        weatherList.shift();
+        this.imgUrl = `${baseUrl}/static/img/weather/${svgFileName}.svg`;
+    }
+}
+
+exports.Weather = Weather;
