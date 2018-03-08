486b843218cb932f033c772cc7d3389b25b18129
danmerus <leoghost@mail.ru>
2018-03-08 16:51:40 +0500

    Delete routes.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const categories = require('./controllers/categories');
-const news = require('./controllers/news');
-
-module.exports = app => {
-    app.get('/', categories.list);
-    app.get('/news/:category([a-z]+)', news.index);
-};
