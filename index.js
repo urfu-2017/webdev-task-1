7b7e35cc3d5e55a7c0865bbf7c0dbaf781f21f5c
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2018-03-10 02:05:03 +0500

    4 arg

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ app.use((req, res, next) => {
 // Подключаем маршруты
 routes(app);
 
-// Фиксируем фатальную ошибку и отправляем ответ с кодом 500
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err.stack);
 
     res.sendStatus(500);
