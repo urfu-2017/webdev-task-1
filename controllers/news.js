214db705bb484d032067af964d404c0452847be4
whoe <who-e@yandex.ru>
2018-03-11 23:56:46 +0500

    News page done.

--- controllers/news.js
+++ controllers/news.js
@@ -2,6 +2,8 @@
 const generic = require('../data/generic');
 const header = require('../data/header');
 const footer = require('../data/footer');
+const { WeatherWidget } = require('../widgets/weather');
+const { NewsModel } = require('../models/news');
 
 exports.news = (req, res) => {
     const data = {};
@@ -9,6 +11,24 @@ exports.news = (req, res) => {
     Object.assign(data, header);
     data.toHomeVisible = true;
     Object.assign(data, footer);
-
-    res.render('news', data);
+    const weatherWidget = new WeatherWidget();
+    const newsModel = new NewsModel();
+    const category = req.params.category;
+    const country = req.query.country === undefined ? 'ru' : req.query.country;
+    let weatherPromise = weatherWidget.getWeather(req)
+        .then((weather) => {
+            Object.assign(data, weather);
+        });
+    let newsPromise = newsModel.getNews(country, category)
+        .then((news) => {
+            Object.assign(data, { news });
+        });
+    Promise.all([newsPromise, weatherPromise])
+        .then(() => {
+            res.render('news', data);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        });
 };
