bde0e09c361907fe6a9f705e3a3a89e81229edd2
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2018-03-11 16:46:49 +0500

    gulp refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,20 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const stylus = require('gulp-stylus');
 
-function buildCss() {
-    return gulp.src('./public/*.styl')
-        .pipe(stylus())
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('./bundle/'));
-}
 gulp.task('copy-imgs', function () {
     return gulp.src('./public/images/*')
         .pipe(gulp.dest('./bundle'));
 });
 
 gulp.task('css', function () {
-    return buildCss();
+    return gulp.src('./public/*.styl')
+        .pipe(stylus())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./bundle/'));
 });
 
 gulp.task('watch-stylus', function () {
     return gulp.watch('./public/*.styl', ['css']);
 });
 
-gulp.task('pack', ['css', 'copy-imgs']);
+gulp.task('default', ['css', 'copy-imgs']);
--- stylus-run.sh
+++ stylus-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gulp css
+npm run css
