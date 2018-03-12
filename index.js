fe13392c2da53e03b9e85872452f892ffbda9e22
ArtyMgn <r.artem74@gmail.com>
2018-03-12 18:53:04 +0500

    removed hbs partials

--- index.js
+++ index.js
@@ -3,18 +3,16 @@
 const path = require('path');
 
 const express = require('express');
-const hbs = require('hbs');
 
 require('dotenv').config();
 
 const routes = require('./routes');
 const weatherMiddleware = require('./middlewares/weather');
 
+const app = express();
+
 const viewsDir = path.join(__dirname, 'views');
 const publicDir = path.join(__dirname, 'public');
-const partialsDir = path.join(viewsDir, 'partials');
-
-const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', viewsDir);
@@ -24,10 +22,8 @@ app.use(weatherMiddleware.getCurrentWeather);
 
 routes(app);
 
-hbs.registerPartials(partialsDir, () => {
-    app.listen(8080, () => {
-        console.info('Open http://localhost:8080/');
-    });
+app.listen(8080, () => {
+    console.info('Open http://localhost:8080/');
 });
 
 module.exports = app;
