e90348fe1184d2ff00f877ce167cb8d968089d8b
ArtyMgn <r.artem74@gmail.com>
2018-03-13 23:30:36 +0500

    change categorioes initialization

--- models/category.js
+++ models/category.js
@@ -1,23 +1,25 @@
 'use strict';
 
-class Category {
-    constructor(link, name, icoSrc, icoAlt) {
-        this.name = name;
-        this.link = link;
-        this.icoSrc = icoSrc;
-        this.icoAlt = icoAlt;
-    }
+const categories = [
+    { link: '/news/general', name: 'Общее', icoSrc: 'icons/text-lines.svg', icoAlt: 'text lines' },
+    { link: '/news/business', name: 'Бизнес', icoSrc: 'icons/money-bag.svg', icoAlt: 'money bag' },
+    { link: '/news/entertainment', name: 'Развлечения',
+        icoSrc: 'icons/smile.svg', icoAlt: 'smile' },
+    { link: '/news/sport', name: 'Спорт', icoSrc: 'icons/soccer.svg', icoAlt: 'ball' },
+    { link: '/news/health', name: 'Здоровье',
+        icoSrc: 'icons/cardiogram.svg', icoAlt: 'cardiogram' },
+    { link: '/news/science', name: 'Наука', icoSrc: 'icons/science.svg', icoAlt: 'science' },
+    { link: '/news/tech', name: 'Технологии', icoSrc: 'icons/rocket.svg', icoAlt: 'rocket' }
+];
 
+class Category {
     static fetchAll(query) {
-        return [
-            new Category(`/news/general?${query}`, 'Общее', 'icons/text-lines.svg', 'text lines'),
-            new Category(`/news/business?${query}`, 'Бизнес', 'icons/money-bag.svg', 'money bag'),
-            new Category(`/news/entertainment?${query}`, 'Развлечения', 'icons/smile.svg', 'smile'),
-            new Category(`/news/sport?${query}`, 'Спорт', 'icons/soccer.svg', 'ball'),
-            new Category(`/news/health?${query}`, 'Здоровье', 'icons/cardiogram.svg', 'cardiogram'),
-            new Category(`/news/science?${query}`, 'Наука', 'icons/science.svg', 'science'),
-            new Category(`/news/tech?${query}`, 'Технологии', 'icons/rocket.svg', 'rocket')
-        ];
+        return categories.map(category => {
+            let { link, name, icoSrc, icoAlt } = category;
+            link = `${link}?${query}`;
+
+            return { link, name, icoSrc, icoAlt };
+        });
     }
 }
 
