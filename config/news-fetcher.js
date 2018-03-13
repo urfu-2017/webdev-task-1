5dd66463779298c8439f7feb374b7c4d8d02ff1f
ftlka <smart.Julia@yandex.ru>
2018-03-13 21:10:58 +0500

    Completely forgot about second model

--- config/news-fetcher.js
+++ config/news-fetcher.js
@@ -1,40 +1,11 @@
 'use strict';
-
-const queryString = require('query-string');
-const formatDate = require('./date-format');
-const fetch = require('node-fetch');
-const keyString = '6ccd50aa452a4d48827db4c4a86077d5';
-const baseUrl = 'https://newsapi.org/v2/top-headlines?';
 const defaultCountry = 'us';
+const NewsModel = require('../models/newsModel');
 
-const getNews = async url => {
-    const response = await fetch(url);
-    const body = await response.json();
-
-    return body.articles;
-};
-
-const getFirstFiveNews = data => {
-    const articles = [];
-    for (let i = 0; i < 5; i++) {
-        const element = data[i];
-        const newsDate = formatDate(element.publishedAt);
-        articles.push({ newsTitle: element.title, newsTime: newsDate,
-            newsContent: element.description, newsSource: element.source.name,
-            newsUrl: element.url, newsImage: element.urlToImage });
-    }
-
-    return articles;
-};
 
 module.exports = async function renderNews(req) {
-    let country = req.query.country;
-    if (!country) {
-        country = defaultCountry;
-    }
-    const category = req.params.category;
-    const url = `${baseUrl}${queryString.stringify({ category, country, apiKey: keyString })}`;
-    const news = await getNews(url);
+    const news = new NewsModel(req.query.country || defaultCountry, req.params.category);
+    const newsData = await news.get();
 
-    return getFirstFiveNews(news);
+    return newsData;
 };
