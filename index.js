cf738e461b68a7513bfbd8427188ab3dff6187ea
Artem Borzov <artem.borz@yandex.ru>
2018-03-13 10:17:54 +0500

    fix hbs could not found problem

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
@@ -9,8 +9,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 require('./routes')(app);
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'), () => {
-    app.listen(8080);
-});
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    partialsDir: [
+        path.join(__dirname, 'views', 'partials')
+    ]
+}));
+app.listen(8080);
 
 module.exports = app;
