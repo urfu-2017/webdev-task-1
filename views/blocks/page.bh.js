45b516fe5ff35eb3b20fcc9855a30d8bb4e1dfe8
Ilya Kulygin <ruserixsian@gmail.com>
2018-03-13 22:57:38 +0500

    Вынесено в переменную title для блока page

--- views/blocks/page.bh.js
+++ views/blocks/page.bh.js
@@ -2,44 +2,45 @@
 
 const config = require('../../config');
 
-module.exports = bh =>
-    bh.match('page', (ctx, json) => {
-        ctx
-            .bem(false)
-            .tag('html')
-            .attrs({ lang: config.views.lang })
-            .content(
-                [
-                    {
-                        tag: 'head',
-                        content: [
-                            {
-                                tag: 'meta',
-                                attrs: { charset: config.views.charset }
-                            },
-                            {
-                                tag: 'title',
-                                content: json.title
-                                    ? `${json.title} | ${config.views.title}` : config.views.title
-                            },
-                            {
-                                tag: 'link',
-                                attrs: {
-                                    rel: 'stylesheet',
-                                    href: config.staticBasePath + 'common.css'
-                                }
+module.exports = bh => bh.match('page', (ctx, json) => {
+    const siteName = config.views.title;
+    const pageTitle = json.title ? `${json.title} | ${siteName}` : siteName;
+
+    ctx
+        .bem(false)
+        .tag('html')
+        .attrs({ lang: config.views.lang })
+        .content(
+            [
+                {
+                    tag: 'head',
+                    content: [
+                        {
+                            tag: 'meta',
+                            attrs: { charset: config.views.charset }
+                        },
+                        {
+                            tag: 'title',
+                            content: pageTitle
+                        },
+                        {
+                            tag: 'link',
+                            attrs: {
+                                rel: 'stylesheet',
+                                href: config.staticBasePath + 'common.css'
                             }
-                        ]
-                    },
-                    {
-                        tag: 'body',
-                        attrs: { class: 'page' },
-                        content: {
-                            elem: 'container',
-                            content: json.content
                         }
+                    ]
+                },
+                {
+                    tag: 'body',
+                    attrs: { class: 'page' },
+                    content: {
+                        elem: 'container',
+                        content: json.content
                     }
-                ],
-                true
-            );
-    });
+                }
+            ],
+            true
+        );
+});
