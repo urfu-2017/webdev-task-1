8efbf226f62fd5326dfd7c3358ea88211388e8b4
Unknown <Danil.Koporushkin@urfu.me>
2018-03-15 00:32:56 +0500

    test2

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -5,13 +5,11 @@ const newsapi = new NewsAPI('7003d399f6ae49cbbd75437b2fb4d33a');
 module.exports = async (req, res1) => {
     let country = req.query.country || 'ru';
     let category = req.params.category;
-    await newsapi.v2.topHeadlines({
+    const article = await newsapi.v2.topHeadlines({
         category: category.toString(),
         language: req.headers['accept-language'],
         country: country.toString()
-    }).then(response => {
-        let article = response;
-        res1.render('news', article);
     });
+    res1.render('news', article);
 };
 
--- index.js
+++ index.js
@@ -43,6 +43,5 @@ app.listen(8080, () => {
 
 exports.error404 = (req, res) => res.sendStatus(404);
 
-exports.settings = handlebarsSettings;
 
 module.exports = app;
