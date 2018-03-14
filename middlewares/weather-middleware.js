c6163933985c67b5911b0befa20dc98e0a25a322
ftlka <smart.Julia@yandex.ru>
2018-03-14 08:40:34 +0500

    Deleted some copypasta, now using weather object instead of res.locals filelds

--- middlewares/weather-middleware.js
+++ middlewares/weather-middleware.js
@@ -5,12 +5,6 @@ const WeatherModel = require('../models/weatherModel');
 module.exports = async (req, res, next) => {
     const weather = new WeatherModel(req.query);
     const weatherData = await weather.get();
-    res.locals.city = weatherData.city;
-    res.locals.image = weatherData.image;
-    res.locals.tempToday = weatherData.tempToday;
-    res.locals.windToday = weatherData.windToday;
-    res.locals.dates = weatherData.dates;
-    res.locals.temps = weatherData.temps;
-    res.locals.winds = weatherData.winds;
+    res.locals.weather = weatherData;
     next();
 };
