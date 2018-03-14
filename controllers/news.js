d9dc7b4b18919a1a22512b241a8deafa5a279636
Reshetnikov Artem <mr.reshetnikov.artem.2017@mail.ru>
2018-03-14 22:29:07 +0500

    Правки

--- controllers/news.js
+++ controllers/news.js
@@ -1,10 +1,11 @@
 'use strict';
-const categories = require('../models/categories');
-const { getNews } = require('../models/news');
+const Categories = require('../models/categories');
+const News = require('../models/news');
 
 async function listNews(req, res) {
     const weatherList = res.locals.weather.weatherList;
-    const news = await getNews(req);
+    const news = await News.getNews(req);
+    const categories = Categories.getCategories();
     const data = { categories, weatherList, news: news.articles };
     res.render('category', data);
 }
--- models/news.js
+++ models/news.js
@@ -3,16 +3,16 @@ const NewsAPI = require('newsapi');
 const config = require('../config');
 const newsapi = new NewsAPI(config.newsApiKey);
 
-function getNews(req) {
-    const country = req.query.country || 'us';
-    const options = {
-        category: req.params.name,
-        language: 'en',
-        country: country,
-        pageSize: 5
-    };
+module.exports = class News {
+    static getNews(req) {
+        const country = req.query.country || 'us';
+        const options = {
+            category: req.params.name,
+            language: 'en',
+            country: country,
+            pageSize: 5
+        };
 
-    return newsapi.v2.topHeadlines(options);
-}
-
-module.exports = { getNews };
+        return newsapi.v2.topHeadlines(options);
+    }
+};
