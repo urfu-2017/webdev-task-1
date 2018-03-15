4bbdd11259ba949b7f22996245da8411af4e4321
NurievMikhail <nurievmm@yandex.ru>
2018-03-15 12:43:34 +0500

    Убрал порт

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
-const config = require('../config');
-
 module.exports = (err, req, res, next) => {
-    const port = config.port || 8080;
-
-    res.render('error', { err: err.stack, port });
+    res.render('error', { err: err.stack });
 };
