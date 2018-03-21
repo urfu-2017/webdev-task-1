bdf0da3f03b591f2f08a8fa63b575d110dbe71ef
AZ404 <ztbzak1@rambler.ru>
2018-03-21 23:45:57 +0500

    tests timeout 10s

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,9 @@ const request = require('supertest');
 
 const app = require('../index');
 
-describe('GET /', () => {
+describe('GET /', function () {
+    // eslint-disable-next-line no-invalid-this
+    this.timeout(10000);
     it('respond with html page', done => {
         request(app)
             .get('/')
